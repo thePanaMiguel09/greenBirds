3306ee28979f333e45cf88dca9490357556ad9c2
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 13:18:47 -0500

    detailResearchScreen added

---
+++ lib/presentation/screens/detail/detail_research_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailResearchScreen extends StatelessWidget {
+  const DetailResearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
