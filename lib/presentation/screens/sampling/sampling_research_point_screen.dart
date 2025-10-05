53e29364b8eaf024f888693097649d729b51e14a
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 13:19:21 -0500

    sampling research point screen added

---
+++ lib/presentation/screens/sampling/sampling_research_point_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SamplingResearchPointScreen extends StatelessWidget {
+  const SamplingResearchPointScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
