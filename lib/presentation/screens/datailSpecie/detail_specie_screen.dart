99c2f082aac81f4d580aad476a91cb7b826ea5f4
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 13:20:00 -0500

    detail species screen

---
+++ lib/presentation/screens/datailSpecie/detail_specie_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailSpecieScreen extends StatelessWidget {
+  const DetailSpecieScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
