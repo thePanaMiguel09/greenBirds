e6e8270f244ee5e124257813dcbcc4e04eb87f1e
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-06 01:21:47 -0500

    getRearchs implemented

--- lib/infraestructure/datasource/research_datasource_imp.dart
+++ lib/infraestructure/datasource/research_datasource_imp.dart
@@ -1,9 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:green_birds/domain/datasource/research_datasource.dart';
 import 'package:green_birds/domain/entities/research.dart';
+import 'package:green_birds/infraestructure/models/research_model.dart';
+import 'package:http/http.dart' as http;
 
 class ResearchDatasourceImp implements ResearchDatasource {
+  final String url = dotenv.env['API_BASE_URL']!;
+  final String token = dotenv.env['TOKEN']!;
+
+  Map<String, String> get headers => {
+    "Content-Type": "application/json",
+    'Authorization': "Bearer $token",
+  };
+
   @override
   Future<List<Research>> getResearchs() async {
-    throw UnimplementedError();
+    final response = await http.get(
+      Uri.parse('$url/research'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      final decoded = json.decode(response.body);
+      final List<dynamic> data = decoded['data'];
+
+      return data
+          .map((json) => ResearchModel.fromJson(json).toResearchEntity())
+          .toList();
+    } else {
+      throw Exception(
+        "Error al obtener investigaciones: ${response.statusCode}",
+      );
+    }
   }
 }
