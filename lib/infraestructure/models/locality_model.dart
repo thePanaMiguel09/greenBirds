fff87efb80745e9322d29f988dc02bf2ba0b053a
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-06 00:12:37 -0500

    locality model

--- lib/infraestructure/models/locality_model.dart
+++ lib/infraestructure/models/locality_model.dart
@@ -1,22 +1,31 @@
 import 'package:green_birds/domain/entities/locality.dart';
 
-class LocalityModel extends Locality {
-  LocalityModel(
-    super.id,
-    super.name,
-    super.village,
-    super.neighborhood,
-    super.city,
-    super.country,
-  );
+class LocalityModel {
+  final String id;
+  final String name;
+  final String village;
+  final String neighborhood;
+  final String city;
+  final String country;
 
+  LocalityModel({
+    required this.id,
+    required this.name,
+    required this.village,
+    required this.neighborhood,
+    required this.city,
+    required this.country,
+  });
 
   factory LocalityModel.fromJson(Map<String, dynamic> json) => LocalityModel(
-    id: json['_id'], 
-    name, 
-    village, 
-    neighborhood, 
-    city, 
-    country)
+    id: json['_id'],
+    name: json['name'],
+    village: json['village'],
+    neighborhood: json['neighborhood'],
+    city: json['city'],
+    country: json['country'],
+  );
 
+  Locality toLocalityEntity() =>
+      Locality(id, name, village, neighborhood, city, country);
 }
