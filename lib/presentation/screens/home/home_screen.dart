9b10b95a0a7064650524dd2b1a5f547a67bbb1db
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-06 17:16:13 -0500

    Statistics view header

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -19,7 +19,75 @@ class HomeScreen extends StatelessWidget {
       ),
       body: researchProvier.initLoading
           ? Text('Cargando')
-          : ResearchHorizontalList(researches: researchProvier.researchs),
+          : Column(
+              children: [
+                ResearchHorizontalList(researches: researchProvier.researchs),
+                SizedBox(height: 20),
+                _StatisticsView(),
+              ],
+            ),
+    );
+  }
+}
+
+class _StatisticsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const color = Color(0xFF26AD71);
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 15),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Estadísticas',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Text('70%'),
+                Text(
+                  'Investigaciones',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Activas',
+                  style: TextStyle(
+                    color: color,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Text('30%'),
+                Text(
+                  'Investigaciones',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Inactivas',
+                  style: TextStyle(
+                    color: color,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
