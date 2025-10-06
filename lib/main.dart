ffa6ed2b8fcafa88817edb44dd7c29d4fecd96f4
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-06 01:41:15 -0500

    Provider added to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:green_birds/infraestructure/datasource/research_datasource_imp.dart';
+import 'package:green_birds/infraestructure/repositories/research_repository_impl.dart';
+import 'package:green_birds/presentation/providers/researchs_provider.dart';
 import 'package:green_birds/presentation/screens/main_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -12,10 +16,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: const MainScreen(),
+    final researchRepository = ResearchRepositoryImpl(
+      researchs: ResearchDatasourceImp(),
+    );
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) =>
+              ResearchsProvider(researchRepository: researchRepository)
+                ..loadResearchs(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: const MainScreen(),
+      ),
     );
   }
 }
