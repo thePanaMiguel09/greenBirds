e982183e3d6b0fad9b52f3e6a4758c461a80666e
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-08 00:58:24 -0500

    samplig poitn card

---
+++ lib/presentation/widgets/sampling_point_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:green_birds/presentation/widgets/custom_chip.dart';
+
+class SamplingPointCard extends StatelessWidget {
+  const SamplingPointCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const color = Color(0xFF26AD71);
+
+    return Card(
+      color: Colors.white,
+      elevation: 4,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                'https://i.pinimg.com/736x/bd/22/1f/bd221fa72326054126cabac9dbc5236c.jpg',
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(width: 5),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Punto de Captura',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: color,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      CustomChip(
+                        icon: Icons.access_time_rounded,
+                        label: '30 Días',
+                      ),
+                      SizedBox(width: 5),
+                      CustomChip(
+                        icon: Icons.circle_outlined,
+                        label: '50 metros',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
