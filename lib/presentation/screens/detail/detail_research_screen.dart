5e83b7b8d8a03c2ecc96f73abb91e00627fa1348
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-07 22:56:30 -0500

    research detail vew header

--- lib/presentation/screens/detail/detail_research_screen.dart
+++ lib/presentation/screens/detail/detail_research_screen.dart
@@ -12,6 +12,96 @@ class DetailResearchScreen extends StatelessWidget {
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
+      body: _ResearchDetailView(),
+    );
+  }
+}
+
+class _ResearchDetailView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const color = Color(0xFF26AD71);
+
+    final mediaSize = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Center(
+          child: Material(
+            borderRadius: BorderRadius.circular(12),
+            elevation: 4,
+            child: Container(
+              width: mediaSize.width * 0.9,
+              height: mediaSize.height * 0.3,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Estudio de Biodiversidad Urbana',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: color,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: color,
+                            ),
+                            width: 12,
+                            height: 12,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Investigación sobre aves en parques de la ciudad',
+                        style: TextStyle(fontSize: 14),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20, left: 20),
+                    child: Row(
+                      spacing: 5,
+                      children: [
+                        Icon(color: color, Icons.flag_outlined),
+                        Text('Objetivo 1'),
+                        Text('Objetivo 2'),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20, left: 20),
+                    child: Row(
+                      spacing: 5,
+                      children: [
+                        Icon(color: color, Icons.check_circle_outline),
+                        Text('Resultado 1'),
+                        Text('Resultado 2'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
