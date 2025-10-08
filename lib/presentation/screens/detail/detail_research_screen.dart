eb230b721b9ff3019ca119336d884f9137e7a3af
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-08 00:59:02 -0500

    sampling points scroll view

--- lib/presentation/screens/detail/detail_research_screen.dart
+++ lib/presentation/screens/detail/detail_research_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:green_birds/presentation/widgets/sampling_point_card.dart';
 
 class DetailResearchScreen extends StatelessWidget {
   const DetailResearchScreen({super.key});
@@ -23,98 +24,107 @@ class _ResearchDetailView extends StatelessWidget {
     const color = Color(0xFF26AD71);
 
     final mediaSize = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Center(
-          child: Material(
-            borderRadius: BorderRadius.circular(12),
-            elevation: 4,
-            child: Container(
-              width: mediaSize.width * 0.9,
-              height: mediaSize.height * 0.3,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Center(
+            child: Material(
+              borderRadius: BorderRadius.circular(12),
+              elevation: 4,
+              child: Container(
+                width: mediaSize.width * 0.9,
+                height: mediaSize.height * 0.3,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Estudio de Biodiversidad Urbana',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: color,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: color,
+                              ),
+                              width: 12,
+                              height: 12,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Investigación sobre aves en parques de la ciudad',
+                          style: TextStyle(fontSize: 14),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20),
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        spacing: 5,
                         children: [
-                          Text(
-                            'Estudio de Biodiversidad Urbana',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: color,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              color: color,
-                            ),
-                            width: 12,
-                            height: 12,
-                          ),
+                          Icon(color: color, Icons.flag_outlined),
+                          Text('Objetivo 1'),
+                          Text('Objetivo 2'),
                         ],
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Investigación sobre aves en parques de la ciudad',
-                        style: TextStyle(fontSize: 14),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20),
+                      child: Row(
+                        spacing: 5,
+                        children: [
+                          Icon(color: color, Icons.check_circle_outline),
+                          Text('Resultado 1'),
+                          Text('Resultado 2'),
+                        ],
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 20),
-                    child: Row(
-                      spacing: 5,
-                      children: [
-                        Icon(color: color, Icons.flag_outlined),
-                        Text('Objetivo 1'),
-                        Text('Objetivo 2'),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 20),
-                    child: Row(
-                      spacing: 5,
-                      children: [
-                        Icon(color: color, Icons.check_circle_outline),
-                        Text('Resultado 1'),
-                        Text('Resultado 2'),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
-        ),
 
-        Padding(
-          padding: EdgeInsets.all(20),
-          child: Row(
-            children: [
-              Icon(Icons.search_outlined, color: color),
-              Text(
-                'Puntos de Muestreo',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-            ],
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Icon(Icons.search_outlined, color: color),
+                Text(
+                  'Puntos de Muestreo',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          ...List.generate(
+            5,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: SamplingPointCard(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
